0da52913aa89cd4d449417ba039f95b7a43cc356
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-10-02 09:13:08 +0530

    added widgets

--- app.py
+++ app.py
@@ -13,3 +13,27 @@ class MyWebBrowser(QMainWindow):
         self.window.setWindowTitle("My simple Web Browser 🌐")
 
         # starting to define ui elements
+        self.layout = QVBoxLayout()
+        self.horizontal = QHBoxLayout()
+
+        # creating url bar
+        self.url_bar = QTextEdit()
+        self.url_bar.setMaximumHeight(30)
+
+        # creating the go button
+        self.go_btn = QPushButton("Go")
+        self.go_btn.setMaximumHeight(30)
+
+        # creating the back button
+        self.back_btn = QPushButton("◁")
+        self.back_btn.setMaximumHeight(30)
+        
+        # creating the forward  button
+        self.forward_btn = QPushButton("▷")
+        self.forward_btn.setMaximumHeight(30)
+
+        # adding the above buttons to horizontal layout
+        self.horizontal.addWidget(self.url_bar)
+        self.horizontal.addWidget(self.go_btn)
+        self.horizontal.addWidget(self.back_btn)
+        self.horizontal.addWidget(self.forward_btn)
